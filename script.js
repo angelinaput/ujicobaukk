5a0233c9ba9bc902429490f59eb09c18895cf136
angelinaput <angelinabologi06@gmail.com>
2024-01-04 09:22:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function insert(num){
+    document.form.textarea.value = document.form.textarea.value + num ;
+}
+
+function clean(){
+    document.form.textarea.value = "";
+}
+
+function equal(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value= eval(hasil);
+}
+
+function back(){
+    var hasil = document.form.textarea.value;
+    document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
